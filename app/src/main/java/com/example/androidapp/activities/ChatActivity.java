57cba2bbc97d0161f392675fdfea25d79cdb0807
namey0112 <70027004+namey0112@users.noreply.github.com>
2024-06-24 00:20:58 +0700

    update tutor filter and chat act resize when soft keyboard appear

--- app/src/main/java/com/example/androidapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/androidapp/activities/ChatActivity.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -76,6 +77,14 @@ public class ChatActivity extends BaseActivity {
         loadReceiverDetails();
         init();
         listenMessages();
+        setAdjustScreen();
+        
+    }
+
+
+    protected void setAdjustScreen() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
     }
 
     private void init() {
